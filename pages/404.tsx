dcf3a8785546f2c9b4b879ee93cfa5c537781f00
Azzi-Mo <alhwarymostafa@gmail.com>
2023-07-11 18:27:48 +0300

    edit

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,9 +8,13 @@ const NotFound = () => {
             router.push("/")
         },3000)
     })
-    return ( <div>Not Found Page 
-        <Link href="/">Home</Link>
-    </div> );
+    return (
+        <div className="not-found">
+          <h1>Ooops...</h1>
+          <h2>That page cannot be found :(</h2>
+          <p>Going back to the <Link href="/"><a>Homepage</a></Link> is 3 seconds...</p>
+        </div>
+      );
 }
  
 export default NotFound ;
