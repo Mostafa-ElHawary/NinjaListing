6291121e832acfa071d23792e9d3d113af6e7872
Azzi-Mo <alhwarymostafa@gmail.com>
2023-07-11 23:53:56 +0300

    edit

---
+++ pages/ninja/[id].tsx
@@ -0,0 +1,97 @@
+
+
+// export const getStaticPaths =async () => {
+    
+//     const res = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const data = await res.json();
+
+//     const paths = data.map(ninja => {
+//         return {
+//             params : {id : ninja.id.toString()}
+//         }
+//     })
+
+    
+//     return {
+//         paths,
+//         fallback:false;
+//     }
+// }
+
+
+
+// export const getStaticProps = async (context) => { 
+//     const id = context.params.id; 
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id); 
+//     const data= await res.json();
+//      return {
+//          props : {ninja : data}
+//         }
+
+//     }
+// const Details = () => {
+//     return ( 
+//     <div> 
+//         <h1>{ ninja.name }</h1> 
+//         <p>{ ninja.email }</p> 
+//         <p>{ ninja.website }</p> 
+//         <p>{ ninja.address.city} </p>
+//     </div> );
+// }
+ 
+// export default Details;
+
+
+import { GetStaticPaths, GetStaticProps } from "next";
+
+interface Ninja {
+  id: number;
+  name: string;
+  email: string;
+  website: string;
+  address: {
+    city: string;
+  };
+}
+
+interface DetailsProps {
+  ninja: Ninja;
+}
+
+const Details = ({ ninja }: DetailsProps) => {
+  return (
+    <div>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
+    </div>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  const paths = data.map((ninja: Ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<DetailsProps> = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+  return {
+    props: { ninja: data },
+  };
+};
+
+export default Details;
--- pages/ninja/index.tsx
+++ pages/ninja/index.tsx
@@ -1,5 +1,13 @@
-import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
-import Styles from '../../styles/Ninja.module.css'
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+  PromiseLikeOfReactNode,
+} from "react";
+import Styles from "../../styles/Ninja.module.css";
+import Link from "next/link";
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
@@ -9,13 +17,22 @@ export const getStaticProps = async () => {
   };
 };
 
-const Index = ({ninjas}) => {
+interface Ninja {
+  id: number;
+  name: string;
+}
+
+interface IndexProps {
+  ninjas: Ninja[];
+}
+
+const Index = ({ ninjas }: IndexProps): ReactElement => {
   return (
     <div>
       <h1>All Ninjas</h1>
-      {ninjas.map(ninja => (
+      {ninjas.map((ninja) => (
         <div key={ninja.id}>
-          <a className={Styles.single}>
+          <a href={`/ninja/${ninja.id}`} className={Styles.single}>
             <h3>{ninja.name}</h3>
           </a>
         </div>
