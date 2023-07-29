2f8ced2bdb501144b927d29e0be6aeda601f454b
Azzi-Mo <alhwarymostafa@gmail.com>
2023-07-30 01:11:17 +0300

    edit

--- pages/secu/index.js
+++ pages/secu/index.js
@@ -1,37 +1,27 @@
-import {
-    Key,
-    ReactElement,
-    JSXElementConstructor,
-    ReactNode,
-    ReactPortal,
-    PromiseLikeOfReactNode,
-  } from "react";
-  import Styles from "../../styles/Secu.module.css";
-  import Link from "next/link";
-  export const getStaticProps = async () => {
+export const getStaticProps = async () => {
     const res = await fetch("http://localhost:4000/teams");
-    const data = await res.json();
+    const data =  await res.json();
   
     return {
       props: { ninjas: data },
     };
   };
+  import Link from "next/link";
+  import React from "react";
   
-
-  const Index = ({ ninjas }) => {
+  function Index({ ninjas }) {
     return (
       <div>
-        <h1>All Teams </h1>
         {ninjas.map((ninja) => (
           <div key={ninja.id}>
-            <a href={`/secu/${ninja.id}`} className={Styles.single}>
-              <h3>{ninja.name}</h3>
-            </a>
+          
+          <Link href={`http://localhost:4000/teams/${ninja.id}`} > {ninja.name }</Link>
+  
           </div>
         ))}
       </div>
     );
-  };
+  }
   
-  export default Index;
+  export default  Index;
   
