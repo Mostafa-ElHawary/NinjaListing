1293fd1aab8a639739455ef0bf3f6f3ee3082862
Azzi-Mo <alhwarymostafa@gmail.com>
2023-08-21 21:05:56 +0300

    edit

---
+++ src/app/components/porfoliocard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function porfoliocard() {
+  return (
+    <div>
+      
+       Portfoliocard
+
+    </div>
+  )
+}
+
+export default porfoliocard
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
-import projectsData from "./projectsdata.json"; 
+import projectsData from "./projectsdata.json";
 import Head from "next/head";
 import Image from "next/image";
 
@@ -12,35 +12,39 @@ export default function Home(TeamId) {
         <link rel="icon" href="/pro.ico" />
       </Head>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-screen-xl mx-auto">
-        {projectsData.map((project , index) => (
-
-          <Link key={project.id} href={`/teams/${project.id}/`} passHref>
+        {projectsData.map((project, index) => (
+          <div key={project.id} href={`/teams/${project.id}/`} passHref>
             <div className="rounded-lg shadow-md overflow-hidden bg-white transform hover:scale-105 transition-transform duration-300 ease-in-out">
               <div className="relative">
-                <img
-                  key={project.id}
-                  src={project.portfolio}
-                  alt="Event"
-                  className="w-full h-40 object-cover"
-                />
+              <img
+                    key={project.id}
+                    src={project.portfolio}
+                    alt="Event"
+                    className="w-full h-40 object-cover"
+                  />
+                  
               </div>
               <div className="p-5">
-              <p className="font-semibold text-gray-800 text-lg mb-2">
-              {project.name}
-            </p>
-            <p className="text-sm text-gray-500 mb-2">
-              {project.techstack}
-            </p>
+                <p className="font-semibold text-gray-800 text-lg mb-2">
+                  {project.name}
+                </p>
+                <p className="text-sm text-gray-500 mb-2">
+                  {project.techstack}
+                </p>
 
-                <div className="flex mb-3 -space-x-2">
-                  {project.images.map((img) => (
-                    <img
-                      key={img.id}
-                      src={img.url}
-                      alt="Team Member"
-                      className="w-6 h-6 rounded-full border-2 border-white"
-                    />
-                  ))}
+                <div >
+
+                <Link className="flex mb-3 -space-x-2" href={`./components/porfoliocard.js`} >
+                
+                {project.images.map((img) => (
+                  <img
+                    key={img.id}
+                    src={img.url}
+                    alt="Team Member"
+                    className="w-6 h-6 rounded-full border-2 border-white"
+                  />
+                ))}
+                </Link>
                 </div>
 
                 <div className="flex justify-between items-center">
@@ -56,8 +60,7 @@ export default function Home(TeamId) {
                 </div>
               </div>
             </div>
-          </Link>
-
+          </div>
         ))}
       </div>
     </main>
