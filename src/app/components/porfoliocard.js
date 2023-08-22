d3b64749c14af29a73d0e2f87aebdc8df931140d
Azzi-Mo <alhwarymostafa@gmail.com>
2023-08-22 08:57:20 +0300

    edit

--- src/app/components/porfoliocard.js
+++ src/app/components/porfoliocard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Porfoliocard({props}) {
+export default function Porfoliocard(props) {
   console.log(props.params);
     return (
     <div>
--- src/app/page.js
+++ src/app/page.js
@@ -13,49 +13,53 @@ export default function Home(TeamId) {
       </Head>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 max-w-screen-xl mx-auto">
         {projectsData.map((project, index) => (
-          <Link key={project.id} href={`/teams/${project.id}/`} passHref>
-            <div className="rounded-lg shadow-md overflow-hidden bg-white transform hover:scale-105 transition-transform duration-300 ease-in-out">
-              <div className="relative">
+          <div key={project.id} className="rounded-lg shadow-md overflow-hidden bg-white transform hover:scale-105 transition-transform duration-300 ease-in-out">
+            <Link href={`/teams/${project.id}/`} passHref>
+              <div >
+                <div className="relative">
+                  <img
+                    key={project.id}
+                    src={project.portfolio}
+                    alt="Event"
+                    className="w-full h-40 object-cover"
+                  />
+                </div>
+                <div className="pt-5 pr-5 pl-5">
+                  <p className="font-semibold text-gray-800 text-lg mb-2">
+                    {project.name}
+                  </p>
+                  <p className="text-sm text-gray-500 mb-2">
+                    {project.techstack}
+                  </p>
+                </div>
+              </div>
+            </Link>
+
+            <div className="pb-5 pr-5 pl-5">
+            <div className="flex mb-3 -space-x-2">
+              {project.images.map((img) => (
                 <img
-                  key={project.id}
-                  src={project.portfolio}
-                  alt="Event"
-                  className="w-full h-40 object-cover"
+                  key={img.id}
+                  src={img.url}
+                  alt="Team Member"
+                  className="w-6 h-6 rounded-full border-2 border-white"
                 />
-              </div>
-              <div className="p-5">
-                <p className="font-semibold text-gray-800 text-lg mb-2">
-                  {project.name}
-                </p>
-                <p className="text-sm text-gray-500 mb-2">
-                  {project.techstack}
-                </p>
-
-                <div className="flex mb-3 -space-x-2">
-                  {project.images.map((img) => (
-                    <img
-                      key={img.id}
-                      src={img.url}
-                      alt="Team Member"
-                      className="w-6 h-6 rounded-full border-2 border-white"
-                    />
-                  ))}
-                </div>
-
-                <div className="flex justify-between items-center">
-                  <div className="flex space-x-2">
-                    <button className="flex items-center text-gray-600 hover:bg-gray-200 rounded-lg px-4 py-1 text-sm">
-                      Members
-                    </button>
-                    <button className="flex items-center text-gray-600 hover:bg-gray-200 rounded-lg px-4 py-1 text-sm">
-                      Edit
-                    </button>
-                  </div>
-                  <p className="text-gray-400 text-sm">{project.date}</p>
-                </div>
-              </div>
+              ))}
             </div>
-          </Link>
+
+            <div className="flex justify-between items-center">
+              <div className="flex space-x-2">
+                <button className="flex items-center text-gray-600 hover:bg-gray-200 rounded-lg px-4 py-1 text-sm">
+                  Members
+                </button>
+                <button className="flex items-center text-gray-600 hover:bg-gray-200 rounded-lg px-4 py-1 text-sm">
+                  Edit
+                </button>
+              </div>
+              <p className="text-gray-400 text-sm">{project.date}</p>
+            </div>
+          </div>
+          </div>
         ))}
       </div>
     </main>
